2d7500479f99f6339a4a4b8036f008a6e37230b9
Luca Saverio Esposito <lucasaverio.esposito@students.uniroma2.eu>
2024-08-05 16:00:13 +0200

    fix the compile bash script

--- LSM/compile_lsm.sh
+++ LSM/compile_lsm.sh
@@ -13,24 +13,26 @@ case "$1" in
         exit 0
         ;;
     *)
-        echo "Starting compile_lsm script" > /dev/kmsg
+        echo "LSM_Installer: Starting compile_lsm script" > /dev/kmsg
 
         # Mount the root filesystem as read-write
         mount -o remount,rw /
 
-        # Ensure the build environment is set up
+       # Ensure the build environment is set up
         if [ ! -d /lib/modules/$(uname -r)/build ]; then
-            echo "Kernel source not found. Exiting." > /dev/kmsg
-            exit 1
+            # Copy kernel headers to initramfs
+            if [ -d "/lib/modules/$(uname -r)/build" ]; then
+                mkdir -p ${DESTDIR}/lib/modules/$(uname -r)
+                cp -r /lib/modules/$(uname -r)/build ${DESTDIR}/lib/modules/$(uname -r)
+                echo "LSM_Installer: Kernel source correctly copied." > /dev/kmsg
+            else
+                echo "LSM_Installer: Kernel source not found. Exiting." > /dev/kmsg
+                exit 1
+            fi
         fi
 
-        # Create a working directory
-        mkdir -p /root/lsm_module
-        cd /root/lsm_module
-
-        # Copy the module source files from their location to the initramfs
-        cp /home/rustxave/Scrivania/Rust-Modules/Rust4Linux/LSM/Makefile .
-        cp /home/rustxave/Scrivania/Rust-Modules/Rust4Linux/LSM/my_lsm.c .
+        # Navigate to the directory with the module source
+        cd /home/rustxave/Scrivania/Rust-Modules/Rust4Linux/LSM
 
         # Compile the module
         make -C /lib/modules/$(uname -r)/build M=$PWD modules
@@ -39,9 +41,9 @@ case "$1" in
         if [ -f my_lsm.ko ]; then
             # Move the compiled module to the appropriate location
             insmod my_lsm.ko
-            echo "Custom LSM module installed and loaded" > /dev/kmsg
+            echo "LSM_Installer: Custom LSM module installed and loaded" > /dev/kmsg
         else
-            echo "Module compilation failed. Exiting." > /dev/kmsg
+            echo "LSM_Installer: Module compilation failed. Exiting." > /dev/kmsg
             exit 1
         fi
         ;;
