63a12b06e7cb3d2558c6ca4dceb94efc5397a249
Luca Saverio Esposito <lucasaverio.esposito@students.uniroma2.eu>
2024-08-05 16:19:30 +0200

    fixed the script

--- LSM/compile_lsm.sh
+++ LSM/compile_lsm.sh
@@ -18,17 +18,20 @@ case "$1" in
         # Mount the root filesystem as read-write
         mount -o remount,rw /
 
-       # Ensure the build environment is set up
-        if [ ! -d /lib/modules/$(uname -r)/build ]; then
+
+        # Ensure the build environment is set up
+        if [ ! -d ${DESTDIR}/lib/modules/$(uname -r)/build ]; then
             # Copy kernel headers to initramfs
-            if [ -d "/lib/modules/$(uname -r)/build" ]; then
+            if [ -d /lib/modules/$(uname -r)/build ]; then
                 mkdir -p ${DESTDIR}/lib/modules/$(uname -r)
                 cp -r /lib/modules/$(uname -r)/build ${DESTDIR}/lib/modules/$(uname -r)
-                echo "LSM_Installer: Kernel source correctly copied." > /dev/kmsg
+                echo "LSM_Installer: Kernel headers copied to initramfs" > /dev/kmsg
             else
-                echo "LSM_Installer: Kernel source not found. Exiting." > /dev/kmsg
+                echo "LSM_Installer: Kernel source not found on the system. Exiting." > /dev/kmsg
                 exit 1
             fi
+        else
+            echo "LSM_Installer: Kernel headers already in initramfs" > /dev/kmsg
         fi
 
         # Navigate to the directory with the module source
